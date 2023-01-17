7494084fe32a6f02846f1b5476cf5030277de9ac
Felipe da Silva <105226190+fesofeng@users.noreply.github.com>
2023-01-17 17:06:28 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     baseUrlBackend: 'http://localhost:8080/api',
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImZlbGlwZSIsImlkIjoxMjM0NTZ9.9vSQXYsxdwAMK6jMaK9umOi-c6kyqVp9ZzsIPL_67fY'
+    token: 'token'
 };
